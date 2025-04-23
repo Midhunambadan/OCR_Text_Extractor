abaa7afb0656e8a4ef259fa5b27121b4576bd042
Midhun Ambadan <midhunambadan06@gmail.com>
2025-04-23 20:41:45 +0530

    small optimization

--- image_to_text/src/app/components/body/body.component.ts
+++ image_to_text/src/app/components/body/body.component.ts
@@ -9,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class BodyComponent {
 
+  str:string=''
+  image:string=''
+  event: any;
+  selectedFile: File | undefined;
+  
+  onSubmit(event:any){
+    this.str='helloooo'
+  }
+  
+  onFileChange(event:any){
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+      // console.log('File selected:', this.selectedFile.name);
+      this.image=this.selectedFile.name
+    }
+  }
+
 }

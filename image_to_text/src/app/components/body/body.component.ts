6395ff3fe5f7e5c54e1f7bc90b02528cc5edd998
Midhun Ambadan <midhunambadan06@gmail.com>
2025-04-27 20:36:38 +0530

    Name added, copy and clear implemented

--- image_to_text/src/app/components/body/body.component.ts
+++ image_to_text/src/app/components/body/body.component.ts
@@ -6,44 +6,60 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './body.component.html',
-  styleUrl: './body.component.css'
+  styleUrl: './body.component.css',
 })
 export class BodyComponent {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient){}
-
-  // str:string=''
-  // image:string=''
   event: any;
   selectedFile: File | undefined;
-  extractedText:string=''
+  extractedText: string = '';
 
-  
-  onFileChange(event:any){
-    this.selectedFile = event.target.files[0]   
+  onFileChange(event: any) {
+    this.selectedFile = event.target.files[0];
     // console.log(this.selectedFile)
   }
 
-
-   
-  onSubmit(event:any){
+  onSubmit(event: any) {
     event.preventDefault();
-    
-    if(!this.selectedFile){
-      console.error('No file selected')
-      return
+
+    if (!this.selectedFile) {
+      console.error('No file selected');
+      return;
     }
 
-    const formData=new FormData()
-    formData.append('file',this.selectedFile)
+    const formData = new FormData();
+    formData.append('file', this.selectedFile);
 
-    this.http.post('http://localhost:5000/extract',formData).subscribe((res:any)=>{
-
-      this.extractedText=res.text
-      console.log('res',res.text)
-    },(error)=>{
-      console.error('Error---',error)
-    })
+    this.http.post('http://localhost:5000/extract', formData).subscribe(
+      (res: any) => {
+        this.extractedText = res.text;
+        console.log('res', res.text);
+      },
+      (error) => {
+        console.error('Error---', error);
+      }
+    );
   }
 
+  copyText() {
+    const textArea: any = document.getElementById('textArea') as HTMLTextAreaElement;
+
+    if (textArea && textArea.value.trim() !== '') {
+      navigator.clipboard
+        .writeText(textArea.value)
+        .then(() => {
+          alert('Text copied successfully!');
+        })
+        .catch((err) => {
+          console.error('Failed to copy text:', err);
+        });
+    } else {
+      alert('No Text found !');
+    }
+  }
+
+  clearText() {
+    this.extractedText = '';
+  }
 }
